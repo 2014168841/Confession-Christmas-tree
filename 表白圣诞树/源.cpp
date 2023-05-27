eb11d726bee8ceddd7539b03456b9c2060d0b859
ligen <2014168841@qq.com>
2023-05-27 21:20:38 +0800

    添加项目文件。

---
+++ 表白圣诞树/源.cpp
@@ -0,0 +1,24 @@
+//1.实现背景
+//2.实现飘舞雪花
+//3.背景音乐
+//4.字幕
+
+#include<graphics.h>//Easyx图形库
+#include<stdio.h>
+int main(void)
+{
+    initgraph(1600, 900);
+
+    IMAGE imgbg, imgSnow;
+
+    loadimage(&imgbg, "res/bj.png");
+    loadimage(&imgSnow, "res/snow.png");
+
+    while (1)
+    {
+        putimage(0, 0, &imgbg);
+        putimage(700, 100, &imgSnow);
+    }
+    system("pause");
+    return 0;
+}
