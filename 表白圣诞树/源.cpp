00a0ce2c5cf4c71fa5d4f41501709268b4524355
ligen <2014168841@qq.com>
2023-05-28 22:49:18 +0800

    2023.5.28

--- 表白圣诞树/源.cpp
+++ 表白圣诞树/源.cpp
@@ -6,23 +6,76 @@
 #include<graphics.h>//Easyx图形库
 #include<stdio.h>
 #include"tools.h"
+#include<time.h>
+#include"vector2.h"
+
+typedef struct snow
+{
+	vector2 strartPoinit;
+	vector2 endPonit;
+	vector2 p1;
+	vector2 p2;
+	float t;
+	vector2 curPos;//当前位置
+}snow;
+
+//定义一个函数对雪花初始化
+void initsnow(struct snow* snow)
+{
+	snow->strartPoinit = vector2(rand() % 1600, -90);
+	snow->endPonit = vector2(rand() % 1600, 940);
+	snow->curPos = snow->strartPoinit;
+	snow->p1 = vector2(snow->strartPoinit.x - (rand() /200 + 200),rand()%200+200);
+	snow->p2 = vector2(snow->endPonit.x + rand() /200 + 200,rand()%100+600);
+	snow->t = 0;
+}
+
+//计算当前雪花的位置
+void calSnowPos(struct snow* snow)
+{
+	snow->t += ((rand() % 3) + static_cast<float>(1)) / 1000;
+	snow->curPos=calcBezierPoint(
+		snow->t,snow->strartPoinit,snow->p1,snow->p2,snow->endPonit);
+	if (snow->t >= 1)
+	{
+		initsnow(snow);
+	}
+}
+
 int main(void)
 {
-    initgraph(1600, 900);
+	//设置随机种子;
+	srand((unsigned int)time(NULL));
 
-    IMAGE imgbg, imgSnow;
+	initgraph(1600, 900);
 
-    loadimage(&imgbg, "res/bj.png");
-    loadimage(&imgSnow, "res/snow.png");
-    int x = 700;
-    int y = 100;
-    while (1)
-    {
-        putimage(0, 0, &imgbg);
-        putimagePNG(x, y, &imgSnow);
-    }
+	IMAGE imgbg, imgSnow;
 
+	loadimage(&imgbg, "res/bj.png");
+	loadimage(&imgSnow, "res/snow.png");
 
-    system("pause");
-    return 0;
+	//定义雪花
+	snow snow1;
+	initsnow(&snow1);
+
+	while (true)
+	{
+		
+		putimage(0, 0, &imgbg);
+		/*putimagePNG(x, y, &imgSnow);*/
+		//y += 4;//往下速度
+		//x += (rand() % 9) - 4;  //-4 +4 (rand() % 9) 0-8 减4就可以得到+-4
+		//y += (rand() % 3) + 2;//2-4；蠕动，不行
+		//需要曲线下落;贝塞尔曲线
+
+		/*putimagePNG2(snow1.curPos.x, snow1.curPos.y,1600, &imgSnow);*/
+		putimage(snow1.curPos.x, snow1.curPos.y, &imgSnow);
+
+		calSnowPos(&snow1);//计算雪花位置
+
+		Sleep(20);
+	}
+
+	system("pause");
+	return 0;
 }
